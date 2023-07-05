ff3b54f676e3f3d28ecd5b18249467c6acec561c
Eli Gladman <8713153+egladman@users.noreply.github.com>
2023-07-05 08:22:53 -0400

    temporarily disable selinux

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ SHELL = bash -o errexit -o pipefail
 export ENABLE_ROOT ?= 1
 export ENABLE_RELEASE ?= 0
 export ENABLE_DEBUG ?= 1
+export ENABLE_SELINUX ?= 0
 
 QEMU_DIR ?= $(abspath qemu)
 BUILD_DIR ?= $(abspath build)
--- fedora/libexec/95-configure-selinux.sh
+++ fedora/libexec/95-configure-selinux.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
 main() {
+    source /etc/selinux/config
+
+    declare -a selinux_envs
+    eval 'selinux_envs=(${!'SELINUX'@})'
+    
+    if [[ $ENABLE_SELINUX -eq 1 ]]; then
+	SELINUX=enforcing
+    else
+	SELINUX=permissive
+    fi
+
+    printf '%s\n' "${selinux_envs[@]}" > /etc/selinux/config    
     restorecon -e /proc -e /sys -e /dev -pR /
 }
 
