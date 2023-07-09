9371bb70a0cf39a8d6d5d2d59b201d55a152bf9c
Eli Gladman <8713153+egladman@users.noreply.github.com>
2023-07-09 18:57:58 -0400

    mount entire systemd dir as tmpfs

--- fedora/libexec/50-fix-systemd.sh
+++ fedora/libexec/50-fix-systemd.sh
@@ -7,6 +7,8 @@ main() {
 	mkdir /var/lib/systemd
     fi
 
+    sed -i '/\/var\/lib\/systemd/d' /etc/rwtab
+
     # Allows systemd-logind to start 
     if [[ ! -f /usr/lib/systemd/systemd-logind ]]; then
 	touch /usr/lib/systemd/systemd-logind
