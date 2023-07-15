ff47254f2205f9564ee86412ea5b0788b337974b
Eli Gladman <8713153+egladman@users.noreply.github.com>
2023-07-14 22:43:07 -0400

    meh

--- fedora/libexec/98-print-summary.sh
+++ fedora/libexec/98-print-summary.sh
@@ -3,14 +3,17 @@
 set -o errexit -o pipefail
 
 main() {
-    log::info "Size of path: /var/lib/containers"
-    du -sh /var/lib/containers
-
     log::info "Installed packages"
     dnf list installed
 
     log::info "Enabled systemd services"
     systemctl --root=/ list-unit-files --type=service --state=enabled
+
+    log::info "Size of path: /var/lib/containers"
+    du -sh /var/lib/containers
+
+    log::info "List images"
+    podman image ls
 }
 
 init
